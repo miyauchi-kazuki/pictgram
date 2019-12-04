248501ac43b6661163480a17ccabfa34d5e03ca2
miyauchi-kazuki <asurukuraro6@icloud.com>
2019-12-04 08:22:07 +0000

    situmonn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true 
   validates :name, length: { in: 1..32 }
   validates :email, presence: true
-  validates :email, fromat: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+    validates :email, fromat: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
   validates :password, presence: true
   validates :password, format: { with: /\A[a-z0-9]+\z/i }, length: { in: 8..32 }
   
