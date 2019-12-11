f402ed03fd2505e389538275ee071c8357399b74
miyauchi-kazuki <asurukuraro6@icloud.com>
2019-12-11 07:20:16 +0000

    syuusei

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ApplicationRecord
   validates :name, presence: true 
   validates :name, length: { in: 1..32 }
   validates :email, presence: true
-    validates :email, fromat: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
   validates :password, presence: true
-  validates :password, format: { with: /\A[a-z0-9]+\z/i }, length: { in: 8..32 }
-  
+  validates :password, format: ' with: /\A(?=.*?[a-z]) (?=.*?\d) [a-z\d] {8,32}+\z/i'
   has_secure_password
 end
